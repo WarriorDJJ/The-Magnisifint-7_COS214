6081eaf906a0af0b1cab7fa72769f25472502c9f
JIMMMEEHHH <jameslake2000@gmail.com>
2021-11-02 14:33:08 +0200

    Initial Clion Project files

---
+++ 214Project/cmake-build-debug/CMakeFiles/214Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "214Project.exe"
+  "214Project.exe.manifest"
+  "214Project.pdb"
+  "CMakeFiles/214Project.dir/main.cpp.obj"
+  "lib214Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/214Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
